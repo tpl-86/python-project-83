7c198a72ab4696e17aeba1402f62b1bfc5d0ed27
tpl-86 <tron-050686@mail.ru>
2025-03-27 08:10:06 +0300

    Changed app.py

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -16,7 +16,7 @@ DATABASE_URL = os.getenv('DATABASE_URL')
 
 
 def get_db_connection():
-    return psycopg2.connect(DATABASE_URL, sslmode="disable")
+    return psycopg2.connect(DATABASE_URL, sslmode="require")
 
 
 @app.route('/', methods=['POST', 'GET'])
